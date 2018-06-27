bba36d1679def8d8354706de8c7201499b5a6c05
Shammi Hans <shammihans1@gmail.com>
2018-06-27 17:44:53 +0530

    https://app.asana.com/0/703676516716940/724298945971406/f , added custom.js

---
+++ src/custom.js
@@ -0,0 +1,22 @@
+
+// $(function () {
+//     console.log('popover loaded');
+//     $('[data-toggle="popover"]').popover( {
+//             'trigger': 'click',
+//             'template': `<div class="popover" role="tooltip">
+//             <div class="arrow"></div>
+//             <h3 class="popover-header">
+//             Search Result Type
+//             </h3>
+//             <div class="popover-body">
+//             lorem epsum
+//             </div>
+//             </div>`
+//         // });
+//     });
+// });
+
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
