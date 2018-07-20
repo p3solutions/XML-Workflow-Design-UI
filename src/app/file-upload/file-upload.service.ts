5643f7dd166c4549981c01f4779b04b47d77d55f
Shammi Hans <shammihans1@gmail.com>
2018-07-20 16:40:02 +0530

    generate & download PDI file

--- src/app/file-upload/file-upload.service.ts
+++ src/app/file-upload/file-upload.service.ts
@@ -8,12 +8,24 @@ import { Observable } from 'rxjs';
 })
 export class FileUploadService {
   private fileUploadUrl = environment.apiUrl + '/files/multi';
+  private generatePDIurl = environment.apiUrl + '/pdigen/upload';
+  private getPDIurl = environment.apiUrl + '/pdigen/download';
+
   constructor(private http: HttpClient) { }
+
   uploadFile(fileToUpload: File[]): Observable<any> {
     const formData: FormData = new FormData();
     fileToUpload.forEach(file => {
       formData.append('files', file, file.name);
     });
     return this.http.post(this.fileUploadUrl, formData);
+  }
+  generatePDI(file): Observable<any> {
+    const formData: FormData = new FormData();
+    formData.append('file', file, file.name);
+    return this.http.post(this.generatePDIurl, formData);
+  }
+  getPDIfile() {
+    return this.http.get(this.getPDIurl, {responseType: 'blob'});
   }
 }
--- src/app/verification-step/verification-step.component.ts
+++ src/app/verification-step/verification-step.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ITreeOptions, TREE_ACTIONS } from 'angular-tree-component';
+import { FileUploadService } from '../file-upload/file-upload.service';
 
 @Component({
   selector: 'app-verification-step',
@@ -22,8 +23,15 @@ export class VerificationStepComponent implements OnInit {
       },
     }
   };
-  loader = false;
-  constructor() { }
+  jsonLoader = false;
+  pdiLoader = false;
+  jsonOutputFileName = 'output_workflow_file.json';
+  xsdFileName = 'pdi-schema.xsd';
+  jsonFileType = 'text/json';
+  xsdFileType = 'text/xml';
+  constructor(
+    private fileUploadService: FileUploadService
+  ) { }
 
   ngOnInit() {
     const configuration = JSON.parse(localStorage.getItem('configuration'));
@@ -45,28 +53,6 @@ export class VerificationStepComponent implements OnInit {
     navProgress[2].classList.add('active');
   }
 
-  downloadFile() {
-    this.loader = true;
-    const dataStr = localStorage.getItem('configuration');
-    if (!dataStr || dataStr.length === 0) {
-      console.log('Error! Can\'t find parsed data.');
-      this.loader = false;
-      return false;
-    }
-    const dataArray = JSON.parse(dataStr);
-    const fileObj = { 'result': dataArray[0] };
-    const jsonTree = JSON.stringify(fileObj, null, 2);
-    const filename = 'output_workflow_file.json';
-    const blob = new Blob([jsonTree], { type: 'text/json' });
-    const e = document.createEvent('MouseEvents');
-    const a = document.createElement('a');
-    a.download = filename;
-    a.href = window.URL.createObjectURL(blob);
-    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':');
-    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-    a.dispatchEvent(e);
-    this.loader = false;
-  }
   colorRHStree() {
     $('.node-wrapper').each((i, el) => {
       let color = '#FFFFFF';
@@ -93,5 +79,53 @@ export class VerificationStepComponent implements OnInit {
       }
       this.colorRHStree();
     }, 100);
+  }
+  downloadFile(content, fileName, fileType) {
+    const type = fileType || 'text';
+    const blob = new Blob([content], { type: type });
+    const e = document.createEvent('MouseEvents');
+    const a = document.createElement('a');
+    a.download = fileName || 'output.txt';
+    a.href = window.URL.createObjectURL(blob);
+    a.dataset.downloadurl = [type, a.download, a.href].join(':');
+    e.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
+    a.dispatchEvent(e);
+  }
+  getJsonFileObj(loader, fileName, type) {
+    const dataStr = localStorage.getItem('configuration');
+    if (!dataStr || dataStr.length === 0) {
+      console.log('Error! Can\'t find parsed data.');
+      loader = false;
+      return false;
+    }
+    const dataArray = JSON.parse(dataStr);
+    const fileObj = { 'result': dataArray[0] };
+    const jsonTree = JSON.stringify(fileObj, null, 2);
+    const name = fileName || 'output_workflow_file.json';
+    const fileType = type || 'text/json';
+    const jsonFileObj: any = {
+      data: jsonTree,
+      name: name,
+      type: fileType
+    };
+    return jsonFileObj;
+  }
+  downloadJSONfile() {
+    this.jsonLoader = true;
+    const jsonFileObj = this.getJsonFileObj(this.jsonLoader, this.jsonOutputFileName, this.jsonFileType);
+    this.downloadFile(jsonFileObj.data, jsonFileObj.name, jsonFileObj.type);
+    this.jsonLoader = false;
+  }
+  downloadPDIfile() {
+    this.pdiLoader = true;
+    const jsonFileObj = this.getJsonFileObj(this.pdiLoader, this.jsonOutputFileName, this.jsonFileType);
+    this.fileUploadService.generatePDI(new File([jsonFileObj.data], jsonFileObj.name)).subscribe( res => {
+      this.fileUploadService.getPDIfile().subscribe(blob => {
+        this.downloadFile(blob, this.xsdFileName, this.xsdFileType);
+        this.pdiLoader = false;
+      });
+    }, (e) => {
+      console.log('ERROR', e);
+    });
   }
 }
